5deaa229ab9ceedbe59c4a8f022a8a032e859596
Manuraptor <majejuma.com>
2023-03-14 13:05:09 +0100

    Actualizacion importante Manu

---
+++ sonidos.js
@@ -0,0 +1,40 @@
+
+function sonido1() {
+   document.getElementById("bombo") .play();
+
+}
+
+function sonido2() {
+   document.getElementById("crash") .play();
+
+}
+
+function sonido3() {
+   document.getElementById("hit-hat") .play();
+
+}
+function sonido4() {
+   document.getElementById("redoblante") .play();
+
+}
+
+
+function sonido5() {
+   document.getElementById("tom1") .play();
+
+}
+
+function sonido6() {
+   document.getElementById("tom2") .play();
+
+}
+
+function sonido7() {
+   document.getElementById("tom3") .play();
+
+}
+
+function sonido8() {
+   document.getElementById("tom4") .play();
+
+}
